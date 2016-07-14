748e91eca1231ef30fc329746dc8dc537706440c
Tyler Long <tyler4long@gmail.com>
2016-07-14 16:53:08 +0800

    Try to debug

---
+++ csharp/test/RC.Tests/Program.cs
@@ -0,0 +1,11 @@
+namespace RingCentral.Test
+{
+    public class Program
+    {
+       public static void Main(string[] args)
+        {
+            var test = new HttpTest();
+            test.GetTest();
+        }
+    }
+}
