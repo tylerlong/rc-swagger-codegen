73281327b703a2ac7018581141442d92b60b2425
Tyler Long <tyler4long@gmail.com>
2016-07-14 13:38:19 +0800

    Add global.json

---
+++ csharp/RC.Test/RestClientTest.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace RringCentral.Test
+{
+    public class RestClientTest
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            var rc = new RingCentral.RC("", "", "");
+            var restapi = rc.Restapi();
+            Assert.NotNull(restapi);
+        }
+    }
+}
