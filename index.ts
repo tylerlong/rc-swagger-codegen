a53905065f963857149b529c8bd913f2f12d7fc7
Tyler Long <tyler4long@gmail.com>
2016-07-14 14:28:30 +0800

    One command to test everything

--- index.ts
+++ index.ts
@@ -11,7 +11,7 @@ const generateModel = (segment) => {
     const models = Array.from(routes.get(segment) || []);
     const methods = Array.from(actions.get(segment) || []);
     const result = env.render("model.cs", { segment, models, methods });
-    fs.writeFileSync(`csharp/src/RC/Generated/${pascalCase(segment)}`, result);
+    fs.writeFileSync(`csharp/src/RC/Generated/${pascalCase(segment)}.cs`, result);
 }
 
 // for (const segment of segments) {
