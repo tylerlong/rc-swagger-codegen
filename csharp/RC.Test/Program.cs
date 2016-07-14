22d22c5a6016e9017111e14111837b565f5ad5b3
Tyler Long <tyler4long@gmail.com>
2016-07-14 11:19:56 +0800

    Add cs project for unit testing

---
+++ csharp/RC.Test/Program.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace RC.Test
+{
+    public class First
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
