230794b2f77a79e7f9258cce0c292a01018b420f
Tyler Long <tyler4long@gmail.com>
2016-07-14 10:41:15 +0800

    Add RC.cs and make it compile

---
+++ csharp/Token.cs
@@ -0,0 +1,14 @@
+namespace RingCentral
+{
+    public partial class Token
+    {
+        public string access_token;
+        public string token_type;
+        public int? expires_in;
+        public string refresh_token;
+        public int? refresh_token_expires_in;
+        public string scope;
+        public string owner_id;
+        public string endpoint_id;
+    }
+}
