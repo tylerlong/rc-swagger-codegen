06b5f95686453c1e0dbd1bfa438bdfcad1e40d23
Tyler Long <tyler4long@gmail.com>
2016-07-16 22:05:34 +0800

    Simplify code

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { swagger, paths, segments, routes, actions, hasIds } from './swagger';
+import { segments, routes, hasIds } from './swagger';
 import * as nunjucks from 'nunjucks';
 import { pascalCase } from 'change-case';
 import * as fs from 'fs';
@@ -12,8 +12,7 @@ env.addFilter('pascalCase', pascalCase);
 
 const generateModel = (segment) => {
     const models = Array.from(routes.get(segment) || []);
-    const methods = Array.from(actions.get(segment) || []);
-    const result = env.render("model.cs", { segment, models, methods, hasIds });
+    const result = env.render("model.cs", { segment, models, hasIds });
     fs.writeFileSync(`csharp/src/RC/Generated/${pascalCase(segment)}.cs`, result);
 }
 
