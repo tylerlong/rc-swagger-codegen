8ce1bae98c89b89227ce6f35bce40ae648e97dd6
Tyler Long <tyler4long@gmail.com>
2016-07-16 20:36:54 +0800

    Some model's constructor doesn't accept _id

--- views/macros.cs
+++ views/macros.cs
@@ -1,3 +1,12 @@
+{% macro model_constructor(segment, hasId) -%}
+{% if hasId %}
+internal {{ segment | pascalCase }}(Model parent, string _id = null) : base(parent, _id) { }
+{% else %}
+internal {{ segment | pascalCase }}(Model parent) : base(parent, null) { }
+{% endif %}
+{%- endmacro %}
+
+
 {% macro model_route(model, hasId) -%}
 {% if hasId %}
 public {{ model | pascalCase }} {{ model | pascalCase }}(string _id = {% if model == 'account' or model == 'extension' %}"~"{% else %}null{% endif %})
@@ -7,7 +16,7 @@ public {{ model | pascalCase }} {{ model | pascalCase }}(string _id = {% if mode
 {% else %}
 public {{ model | pascalCase }} {{ model | pascalCase }}()
 {
-    return new {{ model | pascalCase }}(this, null);
+    return new {{ model | pascalCase }}(this);
 }
 {% endif %}
 {%- endmacro %}
--- views/model.cs
+++ views/model.cs
@@ -4,7 +4,7 @@ namespace RingCentral
 {
     public partial class {{ segment | pascalCase }} : Model
     {
-        internal {{ segment | pascalCase }}(Model parent, string _id = null) : base(parent, _id) { }
+        {{ macros.model_constructor(segment, hasIds.get(segment)) }}
 
         protected override string PathSegment
         {
